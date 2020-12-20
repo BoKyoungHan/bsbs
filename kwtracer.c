aefadeb4657f5ad31b3534b4fd32f6ce14894add
bokyounghan <21500777@handong.edu>
2020-12-20 01:18:17 +0000

    modify kwtracer

--- kwtracer.c
+++ kwtracer.c
@@ -68,8 +68,8 @@ void trace_req_completion(struct pt_regs *ctx, struct request *req)
 	return ;
 }
 
-//void trace_do_user_space_write(struct pt_regs *ctx, struct page *page, struct iov_iter *i, unsigned long offset, size_t btyes)
-int trace_do_user_space_write(struct pt_regs *ctx)
+int trace_do_user_space_write(struct pt_regs *ctx, struct page *page, struct iov_iter *i, unsigned long offset, size_t btyes)
+//int trace_do_user_space_write(struct pt_regs *ctx)
 {
 	return 0;
 
